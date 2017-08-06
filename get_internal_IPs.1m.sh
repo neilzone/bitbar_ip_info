a303dfc175c28552377671238188935728405715
Neil Brown <github@neilzone.co.uk>
2017-08-06 22:01:31 +0100

    Update get_internal_IPs.1m.sh
    Added IPSec functionality
    Mandated colour for all informational elements

--- get_internal_IPs.1m.sh
+++ get_internal_IPs.1m.sh
@@ -3,7 +3,7 @@
 # <bitbar.title>show_IP_info</bitbar.title>
 # <bitbar.author>Neil Brown</bitbar.author>
 # <bitbar.author.github>neilzone</bitbar.author.github>
-# <bitbar.desc>Shows some IP address info and A&A quota remaining</bitbar.desc>
+# <bitbar.desc>Shows current internal IP address(es).</bitbar.desc>
 
 
 # Andrews & Arnold (aaisp) quota collector
@@ -15,11 +15,22 @@
 #PASSWORD=
 #SERVICE=
 
-# By default, this script checks for addresses assigned to the en0 (Wi-Fi) interface. If you want to monitor other interfaces, change en0 to the appropriate interface name
+# By default, this script checks for addresses assigned to the en0 (Wi-Fi) interface and the ipsec0 (IPSec) interface. If you want to monitor other interfaces, change en0 to the appropriate interface name or add other interfaces
 
+
+WiFi_status=$(ifconfig en0 | grep "status: " | awk '{print $2}')
 IPv4_internal=$(ifconfig en0 | grep "inet " | awk '{print $2}')
 IPv6_internal=$(ifconfig en0 | grep "autoconf secured " | awk '{print $2}')
 
+
+# check if there is an IPSec interface and, if so, set variables
+IPSec_status=$(ifconfig | grep "ipsec0")
+if [ ! -z "$IPSec_status" ]; then
+	IPSec_IPv4=$(ifconfig ipsec0 | grep "inet " | awk '{print $2}')
+	IPSec_IPv6=$(ifconfig ipsec0 | grep "inet6" | awk '!/fe80/' | awk '{print $2}')
+fi
+
+# get external IP addresses by pinging external server
 if ping -c 1 ipv4check.neilzone.co.uk &> /dev/null; then
 	IPv4_external=$(curl -s ipv4check.neilzone.co.uk)
 fi
@@ -28,50 +39,68 @@ if ping6 -c 1 ipv6check.neilzone.co.uk &> /dev/null; then
 	IPv6_external=$(curl -s ipv6check.neilzone.co.uk)
 fi
 
+
+# get A&A quota remaining if a login has been specified and if CHAOS2 is reachable
 if [ ! -z "$LOGIN" ]; then
 	if ping -c 1 chaos2.aa.net.uk &> /dev/null; then
 		AA_quota=$(echo "scale=2; $(curl -s --data "control_login=$LOGIN&control_password=$PASSWORD&service=$SERVICE" "https://chaos2.aa.net.uk/broadband/quota/xml" | grep -Eo 'quota-remaining.{0,14}' | awk '{print $2}' FS='"') / 1000^3" | bc)
 	fi
 fi
 
-if [ "$1" = "copyIPv4_internal" ]; then
+# function for adding IP address to clipboard
+
+if [ "$1" = "copyIP" ]; then
   # Copy the IP to clipboard
-  echo -n "$IPv4_internal" | pbcopy
+  echo -n "$2" | pbcopy
 fi
 
-if [ "$1" = "copyIPv6_internal" ]; then
-  # Copy the IP to clipboard
-  echo -n "$IPv6_internal" | pbcopy
-fi
-
-if [ "$1" = "copyIPv4_external" ]; then
-  # Copy the IP to clipboard
-  echo -n "$IPv4_external" | pbcopy
-fi
-
-if [ "$1" = "copyIPv6_external" ]; then
-  # Copy the IP to clipboard
-  echo -n "$IPv6_external" | pbcopy
-fi
-
+# Menubar title
 echo "IP"
 echo "---"
 
-echo "Internal"
-#check if IPv4 address exists
+echo "Wi-Fi"
 
-if [ ! -z "$IPv4_internal" ]; then
-	echo "$IPv4_internal | terminal=false bash='$0' param1=copyIPv4_internal"
+if [ $WiFi_status == "active" ]; then
+
+	#check if IPv4 address exists
+
+	if [ ! -z "$IPv4_internal" ]; then
+		echo "$IPv4_internal | terminal=false bash='$0' param1=copyIP param2=$IPv4_internal"
+	else
+		echo "No IPv4 address | color=black"
+	fi
+
+	#check if IPv6 address exists
+
+	if [ ! -z "$IPv6_internal" ]; then
+		echo "$IPv6_internal | terminal=false bash='$0' param1=copyIP param2=$IPv6_internal"
+	else
+		echo "No IPv6 address | color=black"
+	fi
+	
 else
-	echo "No IPv4 address"
-fi
+	echo "Wi-Fi inactive | color=black"
+fi	
 
-#check if IPv6 address exists
+# IPSec stuff
 
-if [ ! -z "$IPv6_internal" ]; then
-	echo "$IPv6_internal | terminal=false bash='$0' param1=copyIPv6_internal"
+echo "---"
+echo "IPSec"
+
+if [ ! -z "$IPSec_status" ]; then
+
+		if  [ ! -z "$IPSec_IPv4" ]; then
+			echo "$IPSec_IPv4 | terminal=false bash='$0' param1=copyIP param2=$IPSec_IPv4"
+		else
+			echo "No IPv4 address | color=black"
+		fi
+		if  [ ! -z "$IPSec_IPv6" ]; then
+			echo "$IPSec_IPv6 | terminal=false bash='$0' param1=copyIP param2=$IPSec_IPv6"
+		else
+			echo "No IPv4 address | color=black"
+		fi	
 else
-	echo "No IPv6 address"
+	echo "IPSec inactive | color=black"
 fi
 
 echo "---"
@@ -81,21 +110,20 @@ echo "External"
 #check if external IPv4 address exists
 
 if [ ! -z "$IPv4_external" ]; then
-	echo "$IPv4_external | terminal=false bash='$0' param1=copyIPv4_external"
+	echo "$IPv4_external | terminal=false bash='$0' param1=copyIP param2=$IPv4_external"
 else
-	echo "No IPv4 address"
+	echo "No IPv4 address  | color=black"
 fi
 
 #check if external IPv6 address exists
 
+
 if [ ! -z "$IPv6_external" ]; then
-	echo "$IPv6_external | terminal=false bash='$0' param1=copyIPv6_external"
+	echo "$IPv6_external | terminal=false bash='$0' param1=copyIP param2=$IPv6_external"
 else
-	echo "No IPv6 address"
+	echo "No IPv6 address  | color=black"
 fi
 
-
-# optional bit for @aaisp quota. If no login name is specified above (off by default), this section will not show
 
 if [ ! -z "$LOGIN" ]; then
 	echo "---"
@@ -103,6 +131,8 @@ if [ ! -z "$LOGIN" ]; then
 	if [ ! -z "$AA_quota" ]; then
 		echo ""$AA_quota" GB | href=https://control.aa.net.uk"
 	else
-		echo "Unable to get quota"
+		echo "Unable to get quota  | color=black"
 	fi
 fi
+
+
